8df33825bffab2f90ca8813fd84ee6e518fb6736
mohdalmaskhan <arshmurtazakhan@gmail.com>
2026-04-05 22:12:00 +0530

    Added types of loans and a small processing message , still lacks the condition checking

---
+++ LMS.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int choice;
+    printf("\n==============================\n");
+    printf("\n");
+    printf("\n Types Of Loans Available \n");
+    printf("\n");
+    printf("\n==============================\n");
+    printf("\n");
+    printf(" 1- Personal Loan \n 2- Education Loan \n 3- Home Loan \n 4- Vehicle Loan \n 5- Gold Loan \n 6- Business Loan \n 7- Industrial Loan \n 8- Agriculture Loan \n 9- Others \n ");
+    printf("\n");
+    printf("Make Your Choice \n");
+    scanf("%d",&choice);
+    printf("\nProcessing....\n");
+    return 0;
+}
