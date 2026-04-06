dafd127d354bf5237be0ab8bc934b61317a90e6f
nilay007hckkr <nilay00700@gmail.com>
2026-04-06 16:34:55 +0530

    Modification in Menu

--- LMS.c
+++ LMS.c
@@ -1,18 +1,91 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void printLine()
+{
+    printf("  ================================================\n");
+}
+
+void showWelcome()
+{
+    printf("\n\n");
+    printLine();
+    printf("  ||                                            ||\n");
+    printf("  ||           WELCOME TO                       ||\n");
+    printf("  ||      LOAN MANAGEMENT SYSTEM                ||\n");
+    printf("  ||                                            ||\n");
+    printLine();
+    printf("\n");
+    printf("    Simple | Fast | Reliable\n");
+    printf("\n");
+    printLine();
+    printf("\n  Press ENTER to continue . . .");
+
+    getchar();
+
+    system("Cls");
+}
+
+void showProcessing()
+{
+    int i;
+
+    printf("\n  Processing");
+    fflush(stdout);
+
+    for (i = 0; i < 7; i++)
+    {
+        long wait;
+        for (wait = 0; wait < 500000000; wait++)
+            ;
+
+        printf(".");
+        fflush(stdout);
+    }
+
+    printf("  Done!\n");
+}
+
+void showMenu()
+{
+    printf("\n\n");
+    printLine();
+    printf("  ||                                            ||\n");
+    printf("  ||       TYPES OF LOANS AVAILABLE             ||\n");
+    printf("  ||                                            ||\n");
+    printLine();
+    printf("\n");
+    printf("    [ 1 ]  Personal Loan\n");
+    printf("    [ 2 ]  Education Loan\n");
+    printf("    [ 3 ]  Home Loan\n");
+    printf("    [ 4 ]  Vehicle Loan\n");
+    printf("    [ 5 ]  Gold Loan\n");
+    printf("    [ 6 ]  Business Loan\n");
+    printf("    [ 7 ]  Industrial Loan\n");
+    printf("    [ 8 ]  Agriculture Loan\n");
+    printf("    [ 9 ]  Others\n");
+    printf("\n");
+    printLine();
+    printf("\n  Enter Your Choice : ");
+}
+
 int main()
 {
     int choice;
-    printf("\n==============================\n");
+
+    showWelcome();
+    showMenu();
+
+    scanf("%d", &choice);
+
+    showProcessing();
+
     printf("\n");
-    printf("\n Types Of Loans Available \n");
-    printf("\n");
-    printf("\n==============================\n");
-    printf("\n");
-    printf(" 1- Personal Loan \n 2- Education Loan \n 3- Home Loan \n 4- Vehicle Loan \n 5- Gold Loan \n 6- Business Loan \n 7- Industrial Loan \n 8- Agriculture Loan \n 9- Others \n ");
-    printf("\n");
-    printf("Make Your Choice \n");
-    scanf("%d",&choice);
-    printf("\nProcessing....\n");
+    printLine();
+    printf("\n  You selected option [ %d ]\n\n", choice);
+    printLine();
+
     return 0;
 }
